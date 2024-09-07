7680796175b16c7d6c232d00fb2ba3fc018fb280
theManAndHisShadow <justemailpostbox@gmail.com>
2024-09-07 11:23:10 +0500

    [Misc]: minor fix

--- leetcode/JS-TS/top150/array-string/27-removed-element__easy-task.js
+++ leetcode/JS-TS/top150/array-string/27-removed-element__easy-task.js
@@ -23,7 +23,7 @@
  * @return {number}
  */
 const removeElement = function(nums, val) {
-    for(let i = 0; i <= nums.length; i++) {
+    for(let i = 0; i < nums.length; i++) {
         if(nums[i] == val) {
             nums.splice(i, 1);
             i--;
